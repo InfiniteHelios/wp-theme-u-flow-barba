8a442cae6633a0af298627f56b14c3f9afbc3208
helios0722 <90462652+helios0722@users.noreply.github.com>
2014-06-26 13:36:00 -0400

    Code Review - D:/Work/@/wp-theme-u-flow-barba/template-parts/content-parts/050_abstand.php

--- template-parts/content-parts/050_abstand.php
+++ template-parts/content-parts/050_abstand.php
@@ -1,13 +1,1 @@
-<?php
-/**
- * Abstand
- *
- */
-?>
-
-<?php 
-$abstand = get_sub_field('abstand');
-$bg = get_sub_field('hintergrund');
-?>	
-
-<div class="col <?php echo $abstand; ?> <?php echo $bg; ?>">	</div>
+"Thu Jun 26 2014 13:36:00 GMT-0400 (Eastern Daylight Time)" 
